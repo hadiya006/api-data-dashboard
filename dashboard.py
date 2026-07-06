3bdc7dedfdbffb456bf5079f66713bb8ccd8bf72
hadiya006 <hadiyatahir3@gmail.com>
2026-07-06 17:57:55 +0500

    Initialize API dashboard project

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,10 @@
-data = {
-    "name": "Hadiya",
-    "address": {
-        "city": "Lahore",
-        "country": "Pakistan"
-    }
-}
-print(data["address"]["city"])
+import requests
+from pprint import pprint
+
+response=requests.get("https://jsonplaceholder.typicode.com/users")
+data=response.json()
+pprint(data)
+first_user=data[0]
+print(first_user)
+print(type(first_user))
+print(first_user["name"])
